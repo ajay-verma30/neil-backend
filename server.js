995d320dd55cc03734e4ef8ddea54b12182ac39c
Ajay Verma <aryanav30@gmail.com>
2025-11-01 19:27:33 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-
+const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req,res)=>{
