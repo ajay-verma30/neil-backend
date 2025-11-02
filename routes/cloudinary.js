f81fe82c3c1b4061b4f717a689604ac9650bf8ca
Ajay Verma <aryanav30@gmail.com>
2025-11-02 11:31:48 +0530

    Updated cloudinary

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -2,8 +2,8 @@ const { v2: cloudinary } = require("cloudinary");
 
 cloudinary.config({
   cloud_name: "daa5nwpfn",
-  api_key: "777368993482171",
+  api_key: 777368993482171,
   api_secret: "8_5GCAbTccw8TJfU0ZP8p0KcZlk",
 });
 
-module.exports = { cloudinary };
+module.exports =  cloudinary ;
